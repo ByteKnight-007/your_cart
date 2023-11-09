33d7ce1b09dde8ec74eafbe77a0cae5a82dd8ad3
hk-cogoport <kumarharshit8090@gmail.com>
2023-11-10 03:21:13 +0530

    added_authorization

--- src/main/java/com/harshit/yourcartbackend/api/security/WebSecurityConfig.java
+++ src/main/java/com/harshit/yourcartbackend/api/security/WebSecurityConfig.java
@@ -4,13 +4,25 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
+import org.springframework.security.web.authentication.AuthenticationFilter;
 
 @Configuration
 public class WebSecurityConfig {
+    private JWTRequestFilter jwtRequestFilter;
+
+    public WebSecurityConfig(JWTRequestFilter jwtRequestFilter) {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.cors().disable().csrf().disable();
-        http.authorizeHttpRequests().anyRequest().permitAll();
+//        to apply manual filter that is to be executed
+        http.addFilterBefore(jwtRequestFilter, AuthorizationFilter.class);
+
+        http.authorizeHttpRequests()
+                .requestMatchers("/product", "auth/register", "auth/login").permitAll()
+                .anyRequest().authenticated();
 
         return http.build();
     }
